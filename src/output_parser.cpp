7696a65b771889e6cf18047d9cdaca115930e0e7
Jaakko Pyrhönen <jaakkopyrhonen@gmail.com>
2022-12-30 15:21:30 +0200

    työstetty värikoodien parsimista. Ei toimi ihan vielä

--- src/output_parser.cpp
+++ src/output_parser.cpp
@@ -2,52 +2,94 @@
 #include <iostream>
 #include <stdio.h>
 #include <sstream>
+#include <regex>
 #include "output_parser.hpp"
 
-std::vector<std::string> split_by_whitespace(const std::string &out) {
+
+std::vector<std::string> split_by(const std::string &out, char delim) {
     std::vector<std::string> split;
     std::stringstream ss(out);
 
     std::string s;
-    while (getline(ss, s, ' '))
+    while (getline(ss, s, delim))
 	split.push_back(s);
     
     return split;
 }
 
-/* https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
-   
-\033[38;5;206m  ->  \033[38;5;<FG COLOR>m
-\033[48;5;57m   ->  \033[48;5;<BG COLOR>m
-\033[38;2;<r>;<g>;<b>m     #Select RGB foreground color
-\033[48;2;<r>;<g>;<b>m     #Select RGB background color
-
-*/
-   
-// Parse ANSI escape codes for the soon to be rendered text
-std::vector<struct Word> make_words(const std::vector<std::string> &split) {
-    std::vector<struct Word> words;
-
+struct Word parse_to_word(std::string &s) {
     BackgroundColor bg = BackgroundColor::BLACK;
     ForegroundColor fg = ForegroundColor::BLACK;
     DisplayStyle dis = DisplayStyle::NORMAL;
 
+    struct Word w;
+    w.bcolor = bg;
+    w.fcolor = fg;
+    w.style = dis;
+    w.text = "";
+
+    // tämä matchaa oikein. Nice. pitää vielä parsia sen perusteella oikein. Nyt tämä vain crashaa
+    std::string code_string("^.*[[0-9]*([0-9]*;)*([0-9]*)m.*$");
+
+    std::string formatting_code;
+    std::string text;
+    
+    if (std::regex_match(s, std::regex(code_string))) {
+	size_t pos = s.find('[');
+	size_t pos2 = s.find('m');
+
+	if (pos == std::string::npos || pos2 == std::string::npos) {
+	    formatting_code = s.substr(pos, pos2 - pos + 1);
+	    text = s.substr(pos2 - pos + 2, std::string::npos);
+	} else {
+	    text = std::string(s);
+	}
+
+	// koodeja voi olla peräkkäin monta. Halutaan vain teksti talteen
+	while (std::regex_match(text, std::regex(code_string))) {
+	    if (pos == std::string::npos || pos2 == std::string::npos) {
+		break;
+	    }
+	    text = s.substr(pos2 - pos + 2, std::string::npos);
+	}
+
+	
+	//size_t pos = s.find('[');
+	//size_t pos2 = s.find('m');
+	//if (pos != std::string::npos && pos2 != std::string::npos) {
+	//    formatting_code = s.substr(pos, pos2 - pos + 1);
+	//    text = s.substr(pos2 - pos + 2, std::string::npos);
+	//    std::cout << "formatting: " << formatting_code << std::endl;
+	//    std::cout << "text:" << std::endl;
+	//    for (char c : text) {
+	//	std::cout << '\\' << c << std::endl;
+	//    }
+	//}
+    } else {
+	text = std::string(s);
+    }
+    
+    w.text = text;
+    return w;
+}
+
+
+// Parse escape codes for the soon to be rendered text
+std::vector<struct Word> make_words(const std::vector<std::string> &split) {
+    std::vector<struct Word> words;
+    
     // TODO: pitäsi poimia merkkijonoista koodisto ja itse teksti Word-rakenteeseen
-    for (std::string s : split) {	
-	struct Word w = (struct Word) {
-	    bg,
-	    fg,
-	    dis,
-	    std::string(s)
-	};
-	words.push_back(w);
+    for (std::string s : split) {
+	struct Word w = parse_to_word(s);
+	if (!w.text.empty())
+	    words.push_back(w);
     }
     return words;
 }
 
 
 std::vector<struct Word> parse_output(const std::string &out) {
-    const std::vector<std::string> split = split_by_whitespace(out);
+    const std::vector<std::string> split = split_by(out, ' ');
     const std::vector<struct Word> words = make_words(split);
     return words;
 }
--- src/output_parser.hpp
+++ src/output_parser.hpp
@@ -10,57 +10,57 @@
 // esim: \e[0mSomeText\e[0m
 
 enum class BackgroundColor {
-	BLACK = 40,
-	RED,
-	GREEN,
-	YELLOW,
-	BLUE,
-	MAGENTA,
-	CYAN,
-	LIGHTGRAY,
-	GRAY,
-	LIGHTRED,
-	LIGHTGREEN,
-	LIGHTYELLOW,
-	LIGHTBLUE,
-	LIGHTMAGENTA,
-	LIGHTCYAN,
-	WHITE,
+    BLACK = 40,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    LIGHTGRAY,
+    GRAY,
+    LIGHTRED,
+    LIGHTGREEN,
+    LIGHTYELLOW,
+    LIGHTBLUE,
+    LIGHTMAGENTA,
+    LIGHTCYAN,
+    WHITE,
 };
 
 enum class ForegroundColor {
-	BLACK = 30,
-	RED,
-	GREEN,
-	YELLOW,
-	BLUE,
-	MAGENTA,
-	CYAN,
-	LIGHTGRAY,
-	GRAY,
-	LIGHTRED,
-	LIGHTGREEN,
-	LIGHTYELLOW,
-	LIGHTBLUE,
-	LIGHTMAGENTA,
-	LIGHTCYAN,
-	WHITE,
+    BLACK = 30,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    LIGHTGRAY,
+    GRAY,
+    LIGHTRED,
+    LIGHTGREEN,
+    LIGHTYELLOW,
+    LIGHTBLUE,
+    LIGHTMAGENTA,
+    LIGHTCYAN,
+    WHITE,
 };
 
 enum class DisplayStyle {
-	NORMAL = 0,
-	BOLD = 1,
-	ITALIC = 3,
-	UNDERLINE = 4,
-	BLINK = 5,
-	REVERSE = 7
+    NORMAL = 0,
+    BOLD = 1,
+    ITALIC = 3,
+    UNDERLINE = 4,
+    BLINK = 5,
+    REVERSE = 7
 };
 
 struct Word {
-	BackgroundColor bcolor;
-	ForegroundColor fcolor;
-	DisplayStyle style;
-	std::string text;
+    BackgroundColor bcolor;
+    ForegroundColor fcolor;
+    DisplayStyle style;
+    std::string text;
 };
 
 std::vector<struct Word> parse_output(const std::string &out);
